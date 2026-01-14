bb68d77a4e857c88482bd65fbf3ca2797ea3d801
ssw <ssw5712@naver.com>
2026-01-14 14:33:52 +0900

    v1

---
+++ 02_mobile(mega)/js/script.js
@@ -0,0 +1,61 @@
+$(function(){
+
+
+    $('#container section:not('+ $('.submenu li a.connect.selected').attr('href') +')').hide();
+
+
+    $('.submenu li a.connect').click(function(){
+
+        $('#menu_wrap').css({right:'-60%'});
+
+        $('.submenu li a.connect').removeClass('selected');
+        $(this).addClass('selected');
+
+        $('#container section').hide();
+        $( $(this).attr('href')).show();
+        return false;
+    });
+
+
+
+    $('.menu_icon').click(function(){
+        $('#menu_wrap').animate({right:0},"normal");
+        return false;
+    });
+    $('.menu_close').click(function(){
+        $('#menu_wrap').animate({right:'-60%'},"normal");
+        return false;
+    });
+
+    // 웹의 메뉴 숨김 처리
+    $("menu_wrap")
+
+    //메뉴 동작
+    $('#gnb >ul > li').click(function(){
+    //1단 메뉴(on 클래스)
+    $('#gnb > ul > li > a').removeClass('on');
+    $(this).children('a').addClass('on');
+    //2단 메뉴 등장(아코디언)
+    if( $(this).children('ul').css('display') == 'none' ){
+        $('#gnb > ul > li > ul').slideUP();
+    }
+    $(this).children('ul').slideDown();
+
+
+    });
+
+
+
+    //이벤트 => scroll 이벤트 대상(window)
+    $(window).scroll(function(){
+        //스크롤 높이 + 화면 높이
+        let sc_num = $(this).scrollTop();
+        let h_num = $(this).height();
+        //console.log(sc_num, h_num, h_num/2);
+        if( sc_num > h_num/2 ){
+            $('.top_btn').fadeIn();
+        }else{
+            $('.top_btn').fadeOut();
+        }
+    });
+});
