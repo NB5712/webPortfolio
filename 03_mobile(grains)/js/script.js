dbb8d770ef7e7dbb4d1bc066e3e2323242acab1c
ssw <ssw5712@naver.com>
2026-01-28 09:23:48 +0900

    1.1.1

--- 03_mobile(grains)/js/script.js
+++ 03_mobile(grains)/js/script.js
@@ -16,7 +16,13 @@ $(function(){
 
 
 
-
+    $('#gnb > ul > li').click(function(){
+        //클릭한 하위의 submenu가 숨김면 if 실행문 실행
+        if( $(this).children('ul').css('display') == 'none' ){
+            $('.submenu').slideUp();
+        }
+        $(this).children('ul').slideDown();
+    });
 
 
 
